8edb22f14ccde0a50fea89f582cfcb8664ec8afd
Simon Hille <65411985+simonhille@users.noreply.github.com>
2026-07-30 12:24:59 +0200

    Added trivial schmidt values after gutzwiller projecting. (#31)
    added dummy normalized schmidt values in gutzwiller.py after projecting.

--- src/temfpy/gutzwiller.py
+++ src/temfpy/gutzwiller.py
@@ -248,7 +248,14 @@ def abrikosov(
     mps.sites = [spin_site] * mps.L
 
     mps.form = [None] * mps.L
-    mps._S = [None] * (mps.L + 1 if mps.finite else mps.L)
+
+    # TODO: Tenpy at the moment requires the schmidt vectors to be non trivial to determine
+    # TODO: the bond dimensions. As soon as this is fixed, change this to
+    # TODO: mps._S = [None] * (mps.L + 1) if mps.finite else [None] * mps.L
+    legs = [B.get_leg("vL") for B in mps._B]
+    if mps.finite:
+        legs += [mps._B[-1].get_leg("vR")]
+    mps._S = [np.ones(leg.ind_len) / np.sqrt(leg.ind_len) for leg in legs]
 
     logger.info("Completed projection to spin-1/2 space. No conserved charges left.")
 
@@ -260,7 +267,9 @@ def abrikosov(
             case "infinite":
                 mps.canonical_form_infinite1()
             case _:
-                raise NotImplementedError(f"Boundary condition {mps.bc!r} not supported")
+                raise NotImplementedError(
+                    f"Boundary condition {mps.bc!r} not supported"
+                )
         logger.info("Transformed MPS to right canonical form")
     else:
         warn(
@@ -442,7 +451,13 @@ def abrikosov_ph(
     mps.sites = [spin_site] * mps.L
 
     mps.form = [None] * mps.L
-    mps._S = [None] * (mps.L + 1)
+    # TODO: Tenpy at the moment requires the schmidt vectors to be non trivial to determine
+    # TODO: the bond dimensions. As soon as this is fixed, change this to
+    # TODO: mps._S = [None] * (mps.L + 1) if mps.finite else [None] * mps.L
+    legs = [B.get_leg("vL") for B in mps._B]
+    if mps.finite:
+        legs += [mps._B[-1].get_leg("vR")]
+    mps._S = [np.ones(leg.ind_len) / np.sqrt(leg.ind_len) for leg in legs]
 
     logger.info(
         "Completed projection to spin-1/2 space. Conserved charge is now %s",
@@ -457,7 +472,9 @@ def abrikosov_ph(
             case "infinite":
                 mps.canonical_form_infinite1()
             case _:
-                raise NotImplementedError(f"Boundary condition {mps.bc!r} not supported")
+                raise NotImplementedError(
+                    f"Boundary condition {mps.bc!r} not supported"
+                )
         logger.info("Transformed MPS to right canonical form")
     else:
         warn(
